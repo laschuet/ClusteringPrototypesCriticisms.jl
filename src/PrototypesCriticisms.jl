785e8506fb0dd8c9bace7493464b089d4e867ecc
laschuet <laschuet@users.noreply.github.com>
2022-02-16 15:32:15 +0100

    Add approximation of witness function

--- src/PrototypesCriticisms.jl
+++ src/PrototypesCriticisms.jl
@@ -3,11 +3,16 @@ module PrototypesCriticisms
 using KernelFunctions
 using Statistics
 
-export sqmmd, mmd²
+export sqmmd, mmd²,
+        witness
 
 """
 """
 sqmmd(X, Y, k=RBFKernel()) = mean(kernelmatrix(k, X, obsdim=2)) - 2 * mean(kernelmatrix(k, X, Y, obsdim=2)) + mean(kernelmatrix(k, Y, obsdim=2))
 const mmd² = sqmmd
+
+"""
+"""
+witness(z, X, Y, k=RBFKernel()) = mean(map(x -> k(z, x), eachcol(X))) - mean(map(y -> k(z, y), eachcol(Y)))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,14 @@ using Test
         @test sqmmd(X, X) == mmd²(X, X)
         @test sqmmd(X, X) ≈ 0
     end
+
+    @testset "witness" begin
+        X = rand(10, 10)
+        Y = ones(10, 10)
+        x = X[:, 1]
+        y = Y[:, 1]
+        @test witness(x, X, X) ≈ 0
+        @test witness(x, X, Y) > 0
+        @test witness(y, X, Y) < 0
+    end
 end
