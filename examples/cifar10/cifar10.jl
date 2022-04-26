bebc794d95f849daf14f256fcbe66479d24e99e1
laschuet <laschuet@users.noreply.github.com>
2022-04-26 18:42:18 +0200

    Add new example

---
+++ examples/cifar10/cifar10.jl
@@ -0,0 +1,88 @@
+using Clustering
+using Distances
+using FileIO
+using KernelFunctions
+using MLDatasets
+using PrototypesCriticisms
+using Random
+
+"""
+    saveasimage(d, ids; outdir="out")
+    saveasimage(d; kwargs...)
+
+Save the data instances `ids` of the dataset `d` as image files.
+
+If `ids` is unspecified, all data instances of the dataset are saved as image files.
+Assumes that each data instance actually represents an image.
+
+# Keyword arguments
+- `outdir`: the location where the image files are saved.
+
+"""
+function saveasimage(d, ids; outdir="out")
+    mkpath(outdir)
+    for i in ids
+        image = convert2image(d, i)
+        classnames = d.metadata["class_names"]
+        classname = classnames[d.targets[i] + 1]
+        save("$(outdir)/$("0" ^ (length(string(length(d))) - length(string(i))))$(i)_$(classname).png", image)
+    end
+end
+saveasimage(d; kwargs...) = saveasimage(d, 1:length(d); kwargs...)
+
+"""
+    main()
+
+Run the example.
+"""
+function main()
+    Random.seed!(42)
+
+    # Load dataset and the corresponding pre-trained image embeddings
+    dataset = CIFAR10()
+    embedding = load("embedding_train_vgg-19.jld2", "embedding")'
+
+    # Save whole data set as images
+    saveasimage(dataset)
+
+    # Set main program parameters
+    k = 10 # Number of clusters to compute
+    p = 5 # Number of prototypes to find
+    c = 5 # Number of criticisms to find
+
+    # Cluster data using k-medoids
+    clustering = kmedoids(pairwise(Euclidean(), embedding), k)
+
+    # Naive prototypes and criticisms for the clustering
+    protoids = prototypes(clustering, p)
+    critids = criticisms(clustering, c)
+    for i = 1:k
+        saveasimage(dataset, protoids[i], outdir="out/_protos/naive/$i")
+        saveasimage(dataset, critids[i], outdir="out/_crits/naive/$i")
+    end
+
+    # Prototypes and criticisms for the clustering using MMD-critic
+    ys = assignments(clustering)
+    clusters = Vector{Vector{Int}}(undef, k)
+    for i = 1:k
+        clusters[i] = findall(ys .== i)
+    end
+    protoids = Vector{Vector{Int}}(undef, k)
+    critids = Vector{Vector{Int}}(undef, k)
+    for i = 1:k
+        subembedding = view(embedding, :, clusters[i])
+        kernel = with_lengthscale(RBFKernel(), sqrt(size(embedding, 1)))
+        clusterpids = prototypes(subembedding, kernel, p)
+        protoids[i] = clusters[i][clusterpids]
+        clustercids = criticisms(subembedding, kernel, clusterpids, c)
+        critids[i] = clusters[i][clustercids]
+    end
+    for i = 1:k
+        saveasimage(dataset, protoids[i], outdir="out/_protos/mmd-critic/$i")
+        saveasimage(dataset, critids[i], outdir="out/_crits/mmd-critic/$i")
+    end
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
