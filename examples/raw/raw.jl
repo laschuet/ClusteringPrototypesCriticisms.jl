410a439eb5d63794a1bcaa4af35afb128df77db6
laschuet <laschuet@users.noreply.github.com>
2022-05-04 09:41:56 +0200

    Move redundant code to an extra function

--- examples/raw/raw.jl
+++ examples/raw/raw.jl
@@ -6,6 +6,22 @@ using KernelFunctions
 using PrototypesCriticisms
 using Random
 
+function clust(D, protoids, critids, axis, title; color=nothing)
+    println("$title:")
+    println("  Prototypes: ", protoids)
+    println("  Criticisms: ", critids)
+    protos = D[:, first.(protoids)]
+    crits = D[:, first.(critids)]
+    axis.title = title
+    if isnothing(color)
+        scatter!(axis, D[1, :], D[2, :], markersize=5)
+    else
+        scatter!(axis, D[1, :], D[2, :], color=color, markersize=5)
+    end
+    scatter!(axis, protos[1, :], protos[2, :], color=:red, marker=:rect)
+    scatter!(axis, crits[1, :], crits[2, :], marker=:circle)
+end
+
 function main()
     Random.seed!(42)
 
@@ -21,59 +37,31 @@ function main()
     c = 1
 
     fig = Figure()
-    axes = [Axis(fig[i, j], limits=(0, 6, 0, 4), xlabel="x", ylabel="y") for i = 1:2, j = 1:2]
+    axes = [Axis(fig[i, j], xgridvisible=false, ygridvisible=false, limits=(0, 6, 0, 4), xlabel=L"x", ylabel=L"y") for i = 1:2, j = 1:3]
 
     # k-medoids
     clustering = kmedoids(pairwise(Euclidean(), D), k)
     protoids = prototypes(clustering, p)
     critids = criticisms(clustering, c)
-    println("k-medoids:")
-    println("  Prototypes: ", protoids)
-    println("  Criticisms: ", critids)
-    protos = D[:, first.(protoids)]
-    crits = D[:, first.(critids)]
-    scatter!(axes[1], D[1, :], D[2, :])
-    scatter!(axes[1], protos[1, :], protos[2, :])
-    scatter!(axes[1], crits[1, :], crits[2, :])
+    clust(D, protoids, critids, axes[1], "k-medoids", color=assignments(clustering))
 
     # k-means
     clustering = kmeans(D, k)
     protoids = prototypes(clustering, p)
     critids = criticisms(clustering, c)
-    println("k-means:")
-    println("  Prototypes: ", protoids)
-    println("  Criticisms: ", critids)
-    protos = D[:, first.(protoids)]
-    crits = D[:, first.(critids)]
-    scatter!(axes[2], D[1, :], D[2, :])
-    scatter!(axes[2], protos[1, :], protos[2, :])
-    scatter!(axes[2], crits[1, :], crits[2, :])
+    clust(D, protoids, critids, axes[2], "k-means", color=assignments(clustering))
 
     # fuzzy c-means
     clustering = fuzzy_cmeans(D, k, 2)
     protoids = prototypes(clustering, p)
     critids = criticisms(clustering, c)
-    println("fuzzy c-means:")
-    println("  Prototypes: ", protoids)
-    println("  Criticisms: ", critids)
-    protos = D[:, first.(protoids)]
-    crits = D[:, first.(critids)]
-    scatter!(axes[3], D[1, :], D[2, :])
-    scatter!(axes[3], protos[1, :], protos[2, :])
-    scatter!(axes[3], crits[1, :], crits[2, :])
+    clust(D, protoids, critids, axes[3], "fuzzy c-means", color=map(i -> i[2], argmax(clustering.weights, dims=2)))
 
     # MMD-critic
     kernel = with_lengthscale(RBFKernel(), sqrt(size(D, 1)))
     protoids = prototypes(D, kernel, k)
     critids = criticisms(D, kernel, protoids, k)
-    println("MMD-critic:")
-    println("  Prototypes: ", protoids)
-    println("  Criticisms: ", critids)
-    protos = D[:, first.(protoids)]
-    crits = D[:, first.(critids)]
-    scatter!(axes[4], D[1, :], D[2, :])
-    scatter!(axes[4], protos[1, :], protos[2, :])
-    scatter!(axes[4], crits[1, :], crits[2, :])
+    clust(D, protoids, critids, axes[5], "MMD-critic", color=:black)
 
     display(fig)
 end
