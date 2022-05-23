0d2ac15bce84b35e78c7e9ee1f6aa1d691570c29
laschuet <laschuet@users.noreply.github.com>
2022-05-23 15:09:24 +0200

    Produce individual plots

--- examples/raw/raw.jl
+++ examples/raw/raw.jl
@@ -12,20 +12,22 @@ using Random
 
 Print basic text output, and produce plot for the data, prototypes, and criticisms.
 """
-function output(D, protoids, critids, axis, title; color=nothing)
+function output(D, protoids, critids, title; color=nothing)
     println("$title:")
     println("  Prototypes: ", protoids)
     println("  Criticisms: ", critids)
     protos = D[:, vcat(protoids...)]
     crits = D[:, vcat(critids...)]
-    axis.title = title
+    fig = Figure()
+    Axis(fig[1, 1], xgridvisible=false, ygridvisible=false, limits=(0, 6, 0, 4), xlabel=L"x", ylabel=L"y", title=title)
     if isnothing(color)
-        scatter!(axis, D[1, :], D[2, :], markersize=4)
+        scatter!(D[1, :], D[2, :], markersize=4)
     else
-        scatter!(axis, D[1, :], D[2, :], color=color, markersize=4)
+        scatter!(D[1, :], D[2, :], color=color, markersize=4)
     end
-    scatter!(axis, protos[1, :], protos[2, :], color=:red, marker=:cross)
-    scatter!(axis, crits[1, :], crits[2, :], color=:green, marker=:xcross)
+    scatter!(protos[1, :], protos[2, :], color=:red, marker=:cross)
+    scatter!(crits[1, :], crits[2, :], color=:green, marker=:xcross)
+    save("out/$(replace(title, ' ' => '_')).pdf", fig)
 end
 
 """
@@ -49,26 +51,23 @@ function main()
     p = 1
     c = 1
 
-    fig = Figure()
-    axes = [Axis(fig[i, j], xgridvisible=false, ygridvisible=false, limits=(0, 6, 0, 4), xlabel=L"x", ylabel=L"y") for i = 1:2, j = 1:3]
-
     # k-medoids
     clustering = kmedoids(pairwise(Euclidean(), D), k)
     protoids = prototypes(clustering, p)
     critids = criticisms(clustering, c)
-    output(D, protoids, critids, axes[1], "k-medoids", color=assignments(clustering))
+    output(D, protoids, critids, "k-medoids", color=assignments(clustering))
 
     # k-means
     clustering = kmeans(D, k)
     protoids = prototypes(clustering, p)
     critids = criticisms(clustering, c)
-    output(D, protoids, critids, axes[2], "k-means", color=assignments(clustering))
+    output(D, protoids, critids, "k-means", color=assignments(clustering))
 
     # fuzzy c-means
     clustering = fuzzy_cmeans(D, k, 2)
     protoids = prototypes(clustering, p)
     critids = criticisms(clustering, c)
-    output(D, protoids, critids, axes[3], "fuzzy c-means", color=map(i -> i[2], argmax(clustering.weights, dims=2)))
+    output(D, protoids, critids, "fuzzy c-means", color=map(i -> i[2], argmax(clustering.weights, dims=2)))
 
     # affinity propagation
     S = -pairwise(Euclidean(), D, dims=2)
@@ -76,15 +75,13 @@ function main()
     clustering = affinityprop(S)
     protoids = prototypes(clustering, D, p)
     critids = criticisms(clustering, D, c)
-    output(D, protoids, critids, axes[4], "affinity propagation", color=assignments(clustering))
+    output(D, protoids, critids, "affinity propagation", color=assignments(clustering))
 
     # MMD-critic
     kernel = with_lengthscale(RBFKernel(), sqrt(size(D, 1)))
     protoids = prototypes(D, kernel, k)
     critids = criticisms(D, kernel, protoids, k)
-    output(D, protoids, critids, axes[5], "MMD-critic", color=:grey)
-
-    save("out/raw.pdf", fig)
+    output(D, protoids, critids, "MMD-critic", color=:grey)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
