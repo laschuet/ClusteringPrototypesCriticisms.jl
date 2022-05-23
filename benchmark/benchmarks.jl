52b09dff710a2a12ca1a4bbfa266e9c2a4bf87b7
laschuet <laschuet@users.noreply.github.com>
2022-05-23 17:42:10 +0200

    Complete benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -41,31 +41,50 @@ function main()
     Random.seed!(42)
 
     n = 200
-    p = 5
-    c = 5
+    p = 3
+    c = 3
     k = 3
     D = rand(20, n)
-
-    # MMD-critic
-    println("Naive prototypes implementation (MMD-critic)")
-    @btime naiveprototypes($D, $p)
-    println("Prototypes implementation (MMD-critic)")
-    @btime prototypes($D, $p)
-    protoids = rand(1:n, p)
-    println("Naive criticisms implementation (MMD-critic)")
-    @btime naivecriticisms($D, $protoids, $c)
-    println("Criticisms implementation (MMD-critic)")
-    @btime criticisms($D, $protoids, $c)
-
+ 
     # k-medoids
-    c = kmedoids(pairwise(Euclidean(), D), k)
+    clustering = kmedoids(pairwise(Euclidean(), D), k)
     println("Prototypes implementation (k-medoids)")
-    @btime prototypes($c)
+    @btime prototypes($clustering, $p)
+    println("Criticisms implementation (k-medoids)")
+    @btime criticisms($clustering, $c)
 
     # k-means
-    c = kmeans(D, k)
+    clustering = kmeans(D, k)
     println("Prototypes implementation (k-means)")
-    @btime prototypes($c)
+    @btime prototypes($clustering, $p)
     println("Criticisms implementation (k-means)")
-    @btime criticisms($c)
+    @btime criticisms($clustering, $c)
+
+    # fuzzy c-means
+    clustering = fuzzy_cmeans(D, k, 2)
+    println("Prototypes implementation (fuzzy c-means)")
+    @btime prototypes($clustering, $p)
+    println("Criticisms implementation (fuzzy c-means)")
+    @btime criticisms($clustering, $c)
+
+    # affinity propagation
+    S = -pairwise(Euclidean(), D, dims=2)
+    S = S - diagm(0 => diag(S)) + median(S) * I
+    clustering = affinityprop(S)
+    println("Prototypes implementation (affinity propagation)")
+    @btime prototypes($clustering, $D, $p)
+    println("Criticisms implementation (affinity propagation)")
+    @btime criticisms($clustering, $D, $c)
+
+    # MMD-critic
+    kernel = with_lengthscale(RBFKernel(), sqrt(20))
+    println("Naive prototypes implementation (MMD-critic)")
+    @btime naiveprototypes($D, $p, $kernel)
+    println("Prototypes implementation (MMD-critic)")
+    @btime prototypes($D, $kernel, $p)
+    protoids = rand(1:n, p)
+    println("Naive criticisms implementation (MMD-critic)")
+    @btime naivecriticisms($D, $protoids, $c, $kernel)
+    println("Criticisms implementation (MMD-critic)")
+    @btime criticisms($D, $kernel, $protoids, $c)
 end
