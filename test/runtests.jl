a4fab080cc450a7d6681ebf74372ad1facc5ecb8
laschuet <laschuet@users.noreply.github.com>
2022-05-30 11:31:24 +0200

    Add subordinated testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,81 +42,99 @@ end
         @test witness(y, X, Y, k) < 0
     end
 
-    @testset "prototypes" begin
+    @testset verbose=true "prototypes" begin
         n = 50
         X = rand(5, n)
         k = RBFKernel()
         numclusters = 2
 
-        @test length(prototypes(X, k, 0)) == 0
-        @test Set(prototypes(X, k, n)) == Set(1:n)
+        @testset "MMD-critic" begin
+            @test length(prototypes(X, k, 0)) == 0
+            @test Set(prototypes(X, k, n)) == Set(1:n)
+            K = kernelmatrix(k, X, obsdim=2)
+            @test length(prototypes(K, 0)) == 0
+            protoids = prototypes(K, n)
+            @test Set(protoids) == Set(1:n)
+            protoids2 = prototypes(X, ones(Int, n), n, k)
+            @test Set(protoids2...) == Set(1:n)
+            @test collect(protoids2...) == protoids
+        end
 
-        K = kernelmatrix(k, X, obsdim=2)
-        @test length(prototypes(K, 0)) == 0
-        protoids = prototypes(K, n)
-        @test Set(protoids) == Set(1:n)
-        protoids2 = prototypes(X, ones(Int, n), n, k)
-        @test Set(protoids2...) == Set(1:n)
-        @test collect(protoids2...) == protoids
+        @testset "k-medoids" begin
+            c = kmedoids(pairwise(Euclidean(), X, dims=2), numclusters)
+            test(prototypes(c), numclusters, 1)
+            test(prototypes(c, 2), numclusters, 2)
+            test(prototypes(X, assignments(c), 2, :kmedoids), numclusters, 2)
+        end
 
-        c = kmedoids(pairwise(Euclidean(), X, dims=2), numclusters)
-        test(prototypes(c), numclusters, 1)
-        test(prototypes(c, 2), numclusters, 2)
-        test(prototypes(X, assignments(c), 2, :kmedoids), numclusters, 2)
+        @testset "k-means" begin
+            c = kmeans(X, numclusters)
+            test(prototypes(c), numclusters, 1)
+            test(prototypes(c, 2), numclusters, 2)
+            test(prototypes(X, assignments(c), 2, :kmeans), numclusters, 2)
+        end
 
-        c = kmeans(X, numclusters)
-        test(prototypes(c), numclusters, 1)
-        test(prototypes(c, 2), numclusters, 2)
-        test(prototypes(X, assignments(c), 2, :kmeans), numclusters, 2)
+        @testset "fuzzy c-means" begin
+            c = fuzzy_cmeans(X, numclusters, 2)
+            test(prototypes(c), numclusters, 1)
+            test(prototypes(c, 2), numclusters, 2)
+            test(prototypes(X, c.weights, 2, :fuzzycmeans), numclusters, 2)
+        end
 
-        c = fuzzy_cmeans(X, numclusters, 2)
-        test(prototypes(c), numclusters, 1)
-        test(prototypes(c, 2), numclusters, 2)
-        test(prototypes(X, c.weights, 2, :fuzzycmeans), numclusters, 2)
-
-        S = -pairwise(Euclidean(), X, dims=2)
-        S = S - diagm(0 => diag(S)) + median(S) * I
-        c = affinityprop(S)
-        numclusters = nclusters(c)
-        test(prototypes(c, X), numclusters, 1)
-        test(prototypes(c, X, 2), numclusters, 2)
-        test(prototypes(X, assignments(c), 2, :affinitypropagation), numclusters, 2)
+        @testset "affinity propagation" begin
+            S = -pairwise(Euclidean(), X, dims=2)
+            S = S - diagm(0 => diag(S)) + median(S) * I
+            c = affinityprop(S)
+            numclusters = nclusters(c)
+            test(prototypes(c, X), numclusters, 1)
+            test(prototypes(c, X, 2), numclusters, 2)
+            test(prototypes(X, assignments(c), 2, :affinitypropagation), numclusters, 2)
+        end
     end
 
-    @testset "criticisms" begin
+    @testset verbose=true "criticisms" begin
         n = 50
         X = rand(5, n)
         k = RBFKernel()
         numclusters = 2
 
-        @test length(criticisms(X, k, [1, 2], 0)) == 0
-        @test Set(criticisms(X, k, [1, 2], n - 2)) == Set(3:n)
+        @testset "MMD-critic" begin
+            @test length(criticisms(X, k, [1, 2], 0)) == 0
+            @test Set(criticisms(X, k, [1, 2], n - 2)) == Set(3:n)
+            K = kernelmatrix(k, X, obsdim=2)
+            @test length(criticisms(K, [1, 2], 0)) == 0
+            @test Set(criticisms(K, [1, 2], n - 2)) == Set(3:n)
+        end
 
-        K = kernelmatrix(k, X, obsdim=2)
-        @test length(criticisms(K, [1, 2], 0)) == 0
-        @test Set(criticisms(K, [1, 2], n - 2)) == Set(3:n)
+        @testset "k-medoids" begin
+            c = kmedoids(pairwise(Euclidean(), X, dims=2), numclusters)
+            test(criticisms(c), numclusters, 1)
+            test(criticisms(c, 2), numclusters, 2)
+            test(criticisms(X, assignments(c), 2, :kmedoids), numclusters, 2)
+        end
 
-        c = kmedoids(pairwise(Euclidean(), X, dims=2), numclusters)
-        test(criticisms(c), numclusters, 1)
-        test(criticisms(c, 2), numclusters, 2)
-        test(criticisms(X, assignments(c), 2, :kmedoids), numclusters, 2)
+        @testset "k-means" begin
+            c = kmeans(X, numclusters)
+            test(criticisms(c), numclusters, 1)
+            test(criticisms(c, 2), numclusters, 2)
+            test(criticisms(X, assignments(c), 2, :kmeans), numclusters, 2)
+        end
 
-        c = kmeans(X, numclusters)
-        test(criticisms(c), numclusters, 1)
-        test(criticisms(c, 2), numclusters, 2)
-        test(criticisms(X, assignments(c), 2, :kmeans), numclusters, 2)
+        @testset "fuzzy c-means" begin
+            c = fuzzy_cmeans(X, numclusters, 2)
+            test(criticisms(c), numclusters, 1)
+            test(criticisms(c, 2), numclusters, 2)
+            test(criticisms(X, c.weights, 2, :fuzzycmeans), numclusters, 2)
+        end
 
-        c = fuzzy_cmeans(X, numclusters, 2)
-        test(criticisms(c), numclusters, 1)
-        test(criticisms(c, 2), numclusters, 2)
-        test(criticisms(X, c.weights, 2, :fuzzycmeans), numclusters, 2)
-
-        S = -pairwise(Euclidean(), X, dims=2)
-        S = S - diagm(0 => diag(S)) + median(S) * I
-        c = affinityprop(S)
-        numclusters = nclusters(c)
-        test(criticisms(c, X), numclusters, 1)
-        test(criticisms(c, X, 2), numclusters, 2)
-        test(criticisms(X, assignments(c), 2, :affinitypropagation), numclusters, 2)
+        @testset "affinity propagation" begin
+            S = -pairwise(Euclidean(), X, dims=2)
+            S = S - diagm(0 => diag(S)) + median(S) * I
+            c = affinityprop(S)
+            numclusters = nclusters(c)
+            test(criticisms(c, X), numclusters, 1)
+            test(criticisms(c, X, 2), numclusters, 2)
+            test(criticisms(X, assignments(c), 2, :affinitypropagation), numclusters, 2)
+        end
     end
 end
